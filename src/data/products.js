a12959e8581602c318cfe9aa4dc3ff962b52f087
Olatunji <olatee54@gmail.com>
2026-07-10 17:08:08 +0100

    Add description to the products

--- src/data/products.js
+++ src/data/products.js
@@ -94,123 +94,258 @@ const products = [
   },
   {
     id: 6,
-    name: "Beige Cashmere Sweater",
-    price: 120,
-    category: "tops",
-    image: "https://picsum.photos/id/106/600/600",
-    color: "Beige",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "palazzos",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 7,
-    name: "Navy Blue Palazzo Pants",
-    price: 65,
-    category: "palazzo",
-    image: "https://picsum.photos/id/1074/600/600",
-    color: "Navy",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "shoes",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 8,
-    name: "White Crop Top",
-    price: 45,
-    category: "tops",
-    image: "https://picsum.photos/id/64/600/600",
-    color: "White",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "joggers",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 9,
-    name: "Grey Joggers Pants",
-    price: 70,
-    category: "joggers",
-    image: "https://picsum.photos/id/201/600/600",
-    color: "Grey",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "tops",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 10,
-    name: "Black Leather Sneakers",
-    price: 110,
-    category: "shoes",
-    image: "https://picsum.photos/id/180/600/600",
-    color: "Black",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "palazzos",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 11,
-    name: "Pink Wide Leg Palazzo",
-    price: 80,
-    category: "palazzos",
-    image: "https://picsum.photos/id/1011/600/600",
-    color: "Pink",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "shoes",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 12,
-    name: "Red Off-Shoulder Top",
-    price: 55,
-    category: "tops",
-    image: "https://picsum.photos/id/1005/600/600",
-    color: "Red",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "joggers",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 13,
-    name: "Black Cargo Joggers",
-    price: 85,
-    category: "joggers",
-    image: "https://picsum.photos/id/1027/600/600",
-    color: "Black",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "tops",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 14,
-    name: "Brown Chelsea Boots",
-    price: 130,
-    category: "shoes",
-    image: "https://picsum.photos/id/669/600/600",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "joggers",
+    image: BrownHoodie,
     color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 15,
-    name: "Cream Silk Top",
-    price: 60,
+    name: "Brown Oversized Hoodie",
+    price: 35000,
     category: "tops",
-    image: "https://picsum.photos/id/201/600/600",
-    color: "Cream",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 16,
-    name: "Green Palazzo Set",
-    price: 95,
+    name: "Brown Oversized Hoodie",
+    price: 35000,
     category: "palazzos",
-    image: "https://picsum.photos/id/133/600/600",
-    color: "Green",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 17,
-    name: "White Running Sneakers",
-    price: 75,
+    name: "Brown Oversized Hoodie",
+    price: 35000,
     category: "shoes",
-    image: "https://picsum.photos/id/21/600/600",
-    color: "White",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 18,
-    name: "Mustard Yellow Top",
-    price: 50,
-    category: "tops",
-    image: "https://picsum.photos/id/106/600/600",
-    color: "Mustard",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "joggers",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 19,
-    name: "Blue Denim Joggers",
-    price: 78,
-    category: "joggers",
-    image: "https://picsum.photos/id/1060/600/600",
-    color: "Blue",
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "tops",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
   {
     id: 20,
-    name: "Tan Leather Tote Bag",
-    price: 135,
+    name: "Brown Oversized Hoodie",
+    price: 35000,
     category: "palazzos",
-    image: "https://picsum.photos/id/201/600/600",
-    color: "Tan",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
   },
 ];
 
