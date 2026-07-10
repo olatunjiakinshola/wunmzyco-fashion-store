eeadbdda6ead75bbeddbae59a2d8d03d24f6f35d
Olatunji <olatee54@gmail.com>
2026-07-10 13:03:07 +0100

    Add real 5 product pictures and descriptions

--- src/data/products.js
+++ src/data/products.js
@@ -1,25 +1,217 @@
+import BrownHoodie from "../assets/products/BrownHoodie.JPG";
+import BlackGown from "../assets/products/BlackGown.JPG";
+import BlueGown from "../assets/products/BlueGown.JPG";
+import BlackGownn from "../assets/products/BlackGownn.JPG";
+import BlueGownn from "../assets/products/BlueGownn.JPG";
+
 // === 20 PRODUCTS ===
 const products = [
-  { id: 1, name: "Black Oversized Hoodie", price: 75, category: "joggers", image: "https://picsum.photos/id/1015/600/600", color: "Black" },
-  { id: 2, name: "Slim Fit Denim Jeans", price: 95, category: "joggers", image: "https://picsum.photos/id/1060/600/600", color: "Blue" },
-  { id: 3, name: "Brown Leather Crossbody Bag", price: 145, category: "palazzos", image: "https://picsum.photos/id/201/600/600", color: "Brown" },
-  { id: 4, name: "White Minimal Sneakers", price: 85, category: "shoes", image: "https://picsum.photos/id/21/600/600", color: "White" },
-  { id: 5, name: "Canvas Utility Tote Bag", price: 55, category: "palazzos", image: "https://picsum.photos/id/133/600/600", color: "Beige" },
-  { id: 6, name: "Beige Cashmere Sweater", price: 120, category: "tops", image: "https://picsum.photos/id/106/600/600", color: "Beige" },
-  { id: 7, name: "Navy Blue Palazzo Pants", price: 65, category: "palazzo", image: "https://picsum.photos/id/1074/600/600", color: "Navy" },
-  { id: 8, name: "White Crop Top", price: 45, category: "tops", image: "https://picsum.photos/id/64/600/600", color: "White" },
-  { id: 9, name: "Grey Joggers Pants", price: 70, category: "joggers", image: "https://picsum.photos/id/201/600/600", color: "Grey" },
-  { id: 10, name: "Black Leather Sneakers", price: 110, category: "shoes", image: "https://picsum.photos/id/180/600/600", color: "Black" },
-  { id: 11, name: "Pink Wide Leg Palazzo", price: 80, category: "palazzos", image: "https://picsum.photos/id/1011/600/600", color: "Pink" },
-  { id: 12, name: "Red Off-Shoulder Top", price: 55, category: "tops", image: "https://picsum.photos/id/1005/600/600", color: "Red" },
-  { id: 13, name: "Black Cargo Joggers", price: 85, category: "joggers", image: "https://picsum.photos/id/1027/600/600", color: "Black" },
-  { id: 14, name: "Brown Chelsea Boots", price: 130, category: "shoes", image: "https://picsum.photos/id/669/600/600", color: "Brown" },
-  { id: 15, name: "Cream Silk Top", price: 60, category: "tops", image: "https://picsum.photos/id/201/600/600", color: "Cream" },
-  { id: 16, name: "Green Palazzo Set", price: 95, category: "palazzos", image: "https://picsum.photos/id/133/600/600", color: "Green" },
-  { id: 17, name: "White Running Sneakers", price: 75, category: "shoes", image: "https://picsum.photos/id/21/600/600", color: "White" },
-  { id: 18, name: "Mustard Yellow Top", price: 50, category: "tops", image: "https://picsum.photos/id/106/600/600", color: "Mustard" },
-  { id: 19, name: "Blue Denim Joggers", price: 78, category: "joggers", image: "https://picsum.photos/id/1060/600/600", color: "Blue" },
-  { id: 20, name: "Tan Leather Tote Bag", price: 135, category: "palazzos", image: "https://picsum.photos/id/201/600/600", color: "Tan" },
+  {
+    id: 1,
+    name: "Brown Oversized Hoodie",
+    price: 35000,
+    category: "Tops",
+    image: BrownHoodie,
+    color: "Brown",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium oversized hoodie made with comfortable cotton fabric.",
+
+    stock: 15,
+
+    featured: true,
+  },
+
+  {
+    id: 2,
+    name: "Black Oversized Gown",
+    price: 10000,
+    category: "Tops",
+    image: BlackGown,
+    color: "Black",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium Transparent Gown made with comfortable cotton fabric.",
+
+    stock: 10,
+
+    featured: true,
+  },
+  {
+    id: 3,
+    name: "Blue Oversized Gown",
+    price: 20000,
+    category: "Tops",
+    image: BlueGown,
+    color: "Blue",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium Oversized Blue Gown made with comfortable cotton fabric.",
+
+    stock: 20,
+
+    featured: true,
+  },
+  {
+    id: 4,
+    name: "Black Gown",
+    price: 25000,
+    category: "Tops",
+    image: BlackGownn,
+    color: "Black",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium Black Gown with quality fabric.",
+
+    stock: 30,
+
+    featured: true,
+  },
+  {
+    id: 5,
+    name: "Oversized Blue Gown",
+    price: 50000,
+    category: "Tops",
+    image: BlueGownn,
+    color: "Blue",
+
+    sizes: ["M", "L", "XL"],
+
+    description:
+      "Premium Blue Gown with Beautiful Design.",
+
+    stock: 50,
+
+    featured: true,
+  },
+  {
+    id: 6,
+    name: "Beige Cashmere Sweater",
+    price: 120,
+    category: "tops",
+    image: "https://picsum.photos/id/106/600/600",
+    color: "Beige",
+  },
+  {
+    id: 7,
+    name: "Navy Blue Palazzo Pants",
+    price: 65,
+    category: "palazzo",
+    image: "https://picsum.photos/id/1074/600/600",
+    color: "Navy",
+  },
+  {
+    id: 8,
+    name: "White Crop Top",
+    price: 45,
+    category: "tops",
+    image: "https://picsum.photos/id/64/600/600",
+    color: "White",
+  },
+  {
+    id: 9,
+    name: "Grey Joggers Pants",
+    price: 70,
+    category: "joggers",
+    image: "https://picsum.photos/id/201/600/600",
+    color: "Grey",
+  },
+  {
+    id: 10,
+    name: "Black Leather Sneakers",
+    price: 110,
+    category: "shoes",
+    image: "https://picsum.photos/id/180/600/600",
+    color: "Black",
+  },
+  {
+    id: 11,
+    name: "Pink Wide Leg Palazzo",
+    price: 80,
+    category: "palazzos",
+    image: "https://picsum.photos/id/1011/600/600",
+    color: "Pink",
+  },
+  {
+    id: 12,
+    name: "Red Off-Shoulder Top",
+    price: 55,
+    category: "tops",
+    image: "https://picsum.photos/id/1005/600/600",
+    color: "Red",
+  },
+  {
+    id: 13,
+    name: "Black Cargo Joggers",
+    price: 85,
+    category: "joggers",
+    image: "https://picsum.photos/id/1027/600/600",
+    color: "Black",
+  },
+  {
+    id: 14,
+    name: "Brown Chelsea Boots",
+    price: 130,
+    category: "shoes",
+    image: "https://picsum.photos/id/669/600/600",
+    color: "Brown",
+  },
+  {
+    id: 15,
+    name: "Cream Silk Top",
+    price: 60,
+    category: "tops",
+    image: "https://picsum.photos/id/201/600/600",
+    color: "Cream",
+  },
+  {
+    id: 16,
+    name: "Green Palazzo Set",
+    price: 95,
+    category: "palazzos",
+    image: "https://picsum.photos/id/133/600/600",
+    color: "Green",
+  },
+  {
+    id: 17,
+    name: "White Running Sneakers",
+    price: 75,
+    category: "shoes",
+    image: "https://picsum.photos/id/21/600/600",
+    color: "White",
+  },
+  {
+    id: 18,
+    name: "Mustard Yellow Top",
+    price: 50,
+    category: "tops",
+    image: "https://picsum.photos/id/106/600/600",
+    color: "Mustard",
+  },
+  {
+    id: 19,
+    name: "Blue Denim Joggers",
+    price: 78,
+    category: "joggers",
+    image: "https://picsum.photos/id/1060/600/600",
+    color: "Blue",
+  },
+  {
+    id: 20,
+    name: "Tan Leather Tote Bag",
+    price: 135,
+    category: "palazzos",
+    image: "https://picsum.photos/id/201/600/600",
+    color: "Tan",
+  },
 ];
 
 export default products;
